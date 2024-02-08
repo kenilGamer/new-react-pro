3b8dfbf547fcde4c49ccd6bcf949160c59cadf2f
KenilGamer <127214403+kenilGamer@users.noreply.github.com>
2024-02-08 12:45:35 +0530

    q

--- src/components/Produt.jsx
+++ src/components/Produt.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from 'react'
-import Button from './button'
+import Button from './Button'
 
 function Produt({val,mover,index}) {
   return (
